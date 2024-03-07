3bf66ae76e53b75b8a70bab1f73fa363aafecb16
Federico-Tarozzi <161706894+Federico-Tarozzi@users.noreply.github.com>
2024-03-07 17:49:08 +0100

    Create 02_installing packages.R

---
+++ 02_installing packages.R
@@ -0,0 +1,9 @@
+# installing new packages in R
+
+install.packages("terra")
+
+#l'argomento da inserire sarà il pacchetto da installare tra VIRGOLETTE. Il pacchetto va ad essere preso dal cran
+
+library(terra)
+
+#caricare pacchetti in R, se correttamente installati
