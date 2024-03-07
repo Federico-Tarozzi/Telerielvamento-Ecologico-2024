1c878bf14b0c962317d9bb26c919d1e6ed29d872
Federico-Tarozzi <161706894+Federico-Tarozzi@users.noreply.github.com>
2024-03-07 17:05:28 +0100

    Create 01_beginning.R

---
+++ 01_beginning.R
@@ -0,0 +1,31 @@
+# First R script
+
+# R as calculator
+a <-6*7
+b <-5*8
+a + b
+
+# Arrays
+
+flowers <- c(3, 6, 8, 10 , 15, 18)
+
+insects <- c(10, 16, 25, 42, 61, 73)
+
+plot(flowers, insects)
+#changing plot parameters
+
+#symbols
+plot(flowers, insects, pch=19)
+
+#simbols dimention
+plot(flowers, insects, pch=19, cex=2)
+plot(flowers, insects, pch=19, cex=2)
+
+#color
+plot(flowers, insects, pch=19, cex=2, col="chocolate1")
+
+
+
+
+
+
