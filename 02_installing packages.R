a25e64a423664ab846d628b819539318405f7224
Federico Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-03-12 16:50:04 +0100

    Update 02_installing packages.R

--- 02_installing packages.R
+++ 02_installing packages.R
@@ -1,9 +1,21 @@
 # installing new packages in R
 
-install.packages("terra")
-
 #l'argomento da inserire sarà il pacchetto da installare tra VIRGOLETTE. Il pacchetto va ad essere preso dal cran
 
+install.packages("terra")
+
+# al fine di caricare pacchetti in R, se correttamente installati
 library(terra)
 
-#caricare pacchetti in R, se correttamente installati
+
+# volgio installare imageRy da github, volgio quindi prima andare a installare devtools per poter andare ad
+accedere alla funzione install.github
+
+install.packages("devtools")
+
+library(devtools)
+
+# install the imageRy package from GitHub
+devtools::install_github("ducciorocchini/imageRy")
+
+library(imageRy)
