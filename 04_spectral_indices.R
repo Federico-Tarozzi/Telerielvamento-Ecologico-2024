15ba9fb2f87917ff2806084c7e91d573b56b965e
Federico Tarozzi <161706894+federicotarozzi@users.noreply.github.com>
2024-03-26 17:29:18 +0100

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -24,3 +24,29 @@ im.plotRGB(m1992, 2, 1, 3)
 # mostra giallo il blue quindi dove l'uomo ha costruito
 im.plotRGB(m1992, 2, 3, 1)
 
+
+# import neew image of matogrosso in 2006 to highlite the difference between the two images
+m2006<-im.import( "matogrosso_ast_2006209_lrg.jpg")
+
+
+# faccio un par multiframe con 1 riga e 2 colonne
+par(mfrow=c(1,2))
+im.plotRGB(m1992, 1, 2, 3)
+im.plotRGB(m2006, 1, 2, 3)
+
+# vegetazione verde e rosa diventa la terra nuda 
+# nir ontop of green
+im.plotRGB(m2006, 2,1,3)
+
+# nir ontop of blue
+im.plotRGB(m2006, 2,3,1)
+
+# facciamo un mega par con tutte e 6 le immagini trovate, su 2 righe e 3 colonne 
+# multiframe
+par(mfrow=c(2,3))
+im.plotRGB(m1992, 1, 2, 3) # nir on R 1992
+im.plotRGB(m1992, 2, 1, 3) # nir on G 1992
+im.plotRGB(m1992, 2, 3, 1) # nir on B 1992
+im.plotRGB(m2006, 1, 2, 3) # nir on R 2006
+im.plotRGB(m2006, 2, 1, 3) # nir on G 2006
+im.plotRGB(m2006, 2, 3, 1) # nir on B 2006
